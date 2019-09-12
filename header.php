ae022b5b02ed19ca50164d7154a43bc9ae66d44c
Stuart Foulds-Duncan <stuart.duncan@linkgroup.co.uk>
2019-09-12 13:52:00 +0100

    Re-enabled Header Video

--- header.php
+++ header.php
@@ -115,9 +115,9 @@
 				<img src="<?php header_image(); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" class="ui fluid image">
 			<?php else: ?>
 				<video id="featured_video" loop muted autoplay preload="auto" poster="<?php echo get_template_directory_uri(); ?>/assets/img/pho2u.jpg" height="164" width="640">
-					<!-- <source src="<?php echo get_template_directory_uri(); ?>/assets/video/pho2u.mp4" type="video/mp4">
+					<source src="<?php echo get_template_directory_uri(); ?>/assets/video/pho2u.mp4" type="video/mp4">
 					<source src="<?php echo get_template_directory_uri(); ?>/assets/video/pho2u.webm" type="video/webm">
-					<source src="<?php echo get_template_directory_uri(); ?>/assets/video/pho2u.ogv" type="video/ogg"> -->
+					<source src="<?php echo get_template_directory_uri(); ?>/assets/video/pho2u.ogv" type="video/ogg">
 					<img src=="<?php echo get_template_directory_uri(); ?>/assets/img/pho2u.png"/>
 				</video>
 			<?php endif; ?>
